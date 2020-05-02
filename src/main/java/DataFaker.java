d301a3ba3bfe2abb11966a3c0c44a60fc86679a7
VincasBladzius <36604991+VincasBladzius@users.noreply.github.com>
2020-05-02 22:16:36 +0300

    Sukurtas Faker

---
+++ src/main/java/DataFaker.java
@@ -0,0 +1,34 @@
+import com.github.javafaker.Faker;
+
+import java.util.Random;
+
+public class DataFaker {
+
+    Random rand;
+    public DataFaker() {
+        this.rand = new Random();
+    }
+
+    enum Companies{
+        TELEKOMAS, ORLEAN, KLASCO, GRUDAI, AZOTAS, NAFTA, GELEZINKELIAI, TRANSPORTAS, MAXIMA, TRAFFIC;
+    }
+    public String[] GenerateName()
+    {
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String[] fullName = {firstName, lastName};
+        return fullName;
+    }
+
+    public int GenerateIncome(){
+        int income = rand.nextInt(1000);
+        return income;
+    }
+
+    public String GenerateCompany(){
+        int index = rand.nextInt(10);
+        return Companies.values()[index].toString();
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,14 +12,30 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connect = DriverManager.getConnection(url, user, pass);
             statement = connect.createStatement();
-            String query = "SELECT * FROM test";
-            resultSet = statement.executeQuery(query);
+        statement.execute("CREATE TABLE IF NOT EXISTS persons (ID INT PRIMARY KEY AUTO_INCREMENT, " +
+                "first_name TEXT, last_name TEXT, company TEXT, income INT)");
+            DataFaker data = new DataFaker();
+            statement.execute("INSERT INTO persons (first_name, last_name, company, income) VALUES('" + data.GenerateName()[0] + "','" + data.GenerateName()[0] + "'," +
+                    " '" + data.GenerateCompany() + "','" + data.GenerateIncome() + "')");
+
+            statement.execute("SELECT * FROM persons");
+        resultSet = statement.getResultSet();
+
+//        statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 65656654, 'jane@mail.com')");
+//        statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Fido', 65656654, 'fido@mail.com')");
+//           statement.execute("UPDATE contacts SET phone=78787 WHERE name='Joe'");
+//           statement.execute("DELETE FROM contacts WHERE name='Joe'");
+
 
             while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String name = resultSet.getString(2);
-                System.out.println(id + " " + name);
+                String name = resultSet.getString("first_name");
+                String mail = resultSet.getString("last_name");
+                int phone = resultSet.getInt("income");
+                System.out.println(name + " " + phone + " " + mail);
             }
+            resultSet.close();
+            statement.close();
+            connect.close();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
