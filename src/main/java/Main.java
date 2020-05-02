10ad1a2c6b8b34a994a7cfa6e1fda938ea67c3ce
VincasBladzius <36604991+VincasBladzius@users.noreply.github.com>
2020-05-02 23:15:45 +0300

    Isprintinta

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,27 +12,26 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connect = DriverManager.getConnection(url, user, pass);
             statement = connect.createStatement();
-        statement.execute("CREATE TABLE IF NOT EXISTS persons (ID INT PRIMARY KEY AUTO_INCREMENT, " +
-                "first_name TEXT, last_name TEXT, company TEXT, income INT)");
-            DataFaker data = new DataFaker();
-            statement.execute("INSERT INTO persons (first_name, last_name, company, income) VALUES('" + data.GenerateName()[0] + "','" + data.GenerateName()[0] + "'," +
-                    " '" + data.GenerateCompany() + "','" + data.GenerateIncome() + "')");
+            PopulateDatabase populate = new PopulateDatabase(statement);
+                resultSet = populate.CreateDatabase();
 
-            statement.execute("SELECT * FROM persons");
-        resultSet = statement.getResultSet();
 
 //        statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 65656654, 'jane@mail.com')");
 //        statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Fido', 65656654, 'fido@mail.com')");
 //           statement.execute("UPDATE contacts SET phone=78787 WHERE name='Joe'");
 //           statement.execute("DELETE FROM contacts WHERE name='Joe'");
 
-
+            int rowNumber = 1;
             while (resultSet.next()) {
                 String name = resultSet.getString("first_name");
-                String mail = resultSet.getString("last_name");
-                int phone = resultSet.getInt("income");
-                System.out.println(name + " " + phone + " " + mail);
+                String lastName = resultSet.getString("last_name");
+                int income = resultSet.getInt("income");
+                String result = rowNumber + " " + name + " " + lastName + " " + income;
+                rowNumber++;
+                System.out.println(result);
             }
+
+
             resultSet.close();
             statement.close();
             connect.close();
