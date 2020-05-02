c1eb3557a5350381d8c55d6032d3a7f134c1e081
VincasBladzius <36604991+VincasBladzius@users.noreply.github.com>
2020-05-02 16:55:07 +0300

    Ok alright

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class Main {
+    private static Connection connect = null;
+    private static Statement statement = null;
+    private static ResultSet resultSet = null;
+    private static String url = "jdbc:mysql://localhost:3306/pandemic";
+    private static String user = "root", pass = "";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection(url, user, pass);
+            statement = connect.createStatement();
+            String query = "SELECT * FROM test";
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                System.out.println(id + " " + name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
